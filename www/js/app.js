6bfd4e1bd3c8236daabc0d0ca3aad0e49be9d59f
anuarustayev <anuar.ustayev@gmail.com>
2016-07-25 12:10:58 +0600

    tasks are loading when page is opened

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,8 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var db = null;
+var db = null; //declaring db
+var tasks = [];
 var app = angular.module('todo', ['ionic', 'ngCordova'])
 .run(function($ionicPlatform, $cordovaSQLite) {
   $ionicPlatform.ready(function() {
@@ -20,16 +21,14 @@ var app = angular.module('todo', ['ionic', 'ngCordova'])
       StatusBar.styleDefault();
     }
 
+    // Checking if the app is opened in cordova/phonegap or on browser
     if (window.cordova) {
-      console.log("yes, we are in cordova at least...")
-      db = $cordovaSQLite.openDB({ name: "my.db" }); //device
-
+      console.log("yes, we are in cordova...")
+      db = $cordovaSQLite.openDB({ name: "my.db", location: 'default'}); //device
     }else{
-
       db = window.openDatabase("my.db", '1', 'my', 1024 * 1024 * 100); // browser
-
     }
+    // creating table in the my.db database
     $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS todos (id integer primary key autoincrement, task text, done boolean)");
-
   });
 })
--- www/js/controllers/sql.js
+++ www/js/controllers/sql.js
@@ -1,17 +1,19 @@
 'use strict';
 
-app.controller("sqlCtrl", function($scope, $cordovaSQLite){
+app.controller("sqlCtrl", function($scope, $cordovaSQLite, $timeout){
 
+// insert into sqlite db function
   $scope.insert = function(task){
-    var query = "INSERT INTO todos (task) VALUES (?)";
+    var query = "INSERT INTO todos (task) VALUES (?)"; //query in sqlite syntax
     $cordovaSQLite.execute(db, query, [task]).then(function(result){
       console.log("INSERT ID =>" + result.insertId);
     }, function(error){
       console.log(error);
     });
-    $scope.select();
+    $scope.select(); //calling select function to update information displayed
   };
 
+  // this function updates status of a task passing id and new status completed/uncompleted
   $scope.update = function(id,done){
     var query = "UPDATE todos SET done = ? WHERE id = ?";
     $cordovaSQLite.execute(db, query, [done, id]).then(function(result){
@@ -19,9 +21,10 @@ app.controller("sqlCtrl", function($scope, $cordovaSQLite){
     }, function(error){
       console.log(error);
     });
-    $scope.select();
+    $scope.select(); //same as above
   };
 
+  // this function deletes a task using its id
   $scope.delete = function(id){
     var query = "DELETE FROM todos WHERE id = ?";
     $cordovaSQLite.execute(db, query, [id]).then(function(result){
@@ -29,25 +32,27 @@ app.controller("sqlCtrl", function($scope, $cordovaSQLite){
     }, function(error){
       console.log(error);
     });
-    $scope.select();
+    $scope.select(); //same as above
   };
 
+  // function to retreiving data from sqlite db, it selects all existing tasks
   $scope.select = function(){
-    $scope.todoList = [];
-    $scope.todoDone = [];
-    var query = "SELECT id, task, done FROM todos";
+    $scope.todoList = []; //initializing uncompleted tasks array
+    $scope.todoDone = []; //initializing completed tasks array
+    var query = "SELECT id, task, done FROM todos"; //query to select all tasks
     $cordovaSQLite.execute(db, query).then(function(result){
-      if(result.rows.length > 0){
-        for(var i=0;i<result.rows.length;i++) {
+      if(result.rows.length > 0){ //checking if there any rows in db
+        for(var i=0;i<result.rows.length;i++) { //if so then itirate all rows
           console.log("SELECTED =>" + result.rows.item(i).task + " " + result.rows.item(i).id);
-          if(result.rows.item(i).done === 'true'){
+          if(result.rows.item(i).done === 'true'){ // if task is already completed then put it into completed tasks
             $scope.todoDone.unshift({id:result.rows.item(i).id,
                                     task:result.rows.item(i).task,
                                     done:result.rows.item(i).done});
-          } else {
+          } else { //if task is not completed yet then put it into due tasks
             $scope.todoList.unshift({id:result.rows.item(i).id,
                                     task:result.rows.item(i).task,
-                                    done:result.rows.item(i).done})
+                                    done:result.rows.item(i).done
+                                    })
           }
         }
 
@@ -56,5 +61,10 @@ app.controller("sqlCtrl", function($scope, $cordovaSQLite){
       console.log(error);
     });
   };
+
+  $timeout(function () {
+    $scope.select();
+  }, 1000);
+
 
 });
